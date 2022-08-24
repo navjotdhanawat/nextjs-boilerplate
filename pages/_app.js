1e8ce87eaaf09b15dd4a6a2a567210ee8d7579e0
Navjot Dhanawat <navjotdhanawat@gmail.com>
2022-08-24 18:25:20 +0100

    setup tailwind and material-tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 
+import { ThemeProvider } from "@material-tailwind/react";
+
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
 
-  return <>{getLayout(<Component {...pageProps} />)}</>;
+  return (
+    <ThemeProvider>
+      <>{getLayout(<Component {...pageProps} />)}</>
+    </ThemeProvider>
+  );
 }
